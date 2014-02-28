743f0a608f8e4ab130450feb3face585939e6194
Robert Scholte <rfscholte@apache.org>
2014-02-28 11:56:58 +0000

    Add a lot of ITs
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/consumer-maven-plugin@19484 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/projects/os-profile-active/verify.groovy
+++ src/it/projects/os-profile-active/verify.groovy
@@ -16,17 +16,18 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 File originalPom = new File( basedir, 'pom.xml' )
 assert originalPom.exists()
- 
+
 def originalProject = new XmlSlurper().parse( originalPom )
-assert '${depVersion}' == originalProject.dependencies.dependency.version.text()
-assert '${utilVersion}' == originalProject.profiles.profile.dependencies.dependency.version.text()
+assert 0 ==  originalProject.dependencies.size()
+assert 'os' == originalProject.profiles.profile.id.text()
+assert 1 == originalProject.profiles.profile.dependencies.size()
 
 File consumerPom = new File( basedir, 'target/consumer-pom.xml' )
 assert consumerPom.exists()
 
 def consumerProject = new XmlSlurper().parse( consumerPom )
-assert '1.1' == consumerProject.dependencies.dependency.version.text()
-assert '3.2.1' == consumerProject.profiles.profile.dependencies.dependency.version.text()
+assert 0 ==  consumerProject.dependencies.size()
+assert 'os' == consumerProject.profiles.profile.id.text()
+assert 1 == consumerProject.profiles.profile.dependencies.size()

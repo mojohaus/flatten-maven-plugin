3c6796571eb8edd0a2dea33a70ab8e64b2f098fb
Jörg Hohwiller <joerg.hohwiller@capgemini-sdm.com>
2014-03-06 19:21:31 +0000

    added IT for license header
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/consumer-maven-plugin@19499 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/projects/xml-header-comment-present/verify.groovy
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+File originalPom = new File( basedir, 'pom.xml' )
+assert originalPom.exists()
+
+def originalPomContent = originalPom.getText('UTF-8')
+def originalHeader = originalPomContent.substring(0, originalPomContent.indexOf('<project'))
+def commentEndIndex = originalHeader.indexOf('-->')
+assert commentEndIndex > 0;
+def commentStartIndex = originalHeader.indexOf('<!--')
+assert commentStartIndex > 0;
+originalHeader = originalHeader.substring(commentStartIndex, commentEndIndex).replaceAll('\r','')
+assert originalHeader.contains('Licensed to the Apache Software Foundation (ASF)')
+assert originalHeader.contains('http://www.apache.org/licenses/LICENSE-2.0')
+assert originalHeader.contains('under the License.')
+
+File consumerPom = new File( basedir, 'target/consumer-pom.xml' )
+assert consumerPom.exists()
+
+def consumerPomContent = consumerPom.getText('UTF-8')
+def consumerHeader = consumerPomContent.substring(0, consumerPomContent.indexOf('<project'))
+commentEndIndex = consumerHeader.indexOf('-->')
+assert commentEndIndex > 0
+commentStartIndex = consumerHeader.indexOf('<!--')
+assert commentStartIndex > 0;
+consumerHeader = consumerHeader.substring(commentStartIndex, commentEndIndex).replaceAll('\r','')
+assert consumerHeader == originalHeader

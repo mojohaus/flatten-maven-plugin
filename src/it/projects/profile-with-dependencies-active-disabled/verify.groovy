f98dd3f93549d3485ae138900f9544fd79113250
Joerg Hohwiller <hohwille@users.sourceforge.net>
2015-11-30 22:55:56 +0100

    #14: added ITs for profile scenarios

---
+++ src/it/projects/profile-with-dependencies-active-disabled/verify.groovy
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+File originalPom = new File( basedir, 'pom.xml' )
+assert originalPom.exists()
+
+def originalProject = new XmlSlurper().parse( originalPom )
+assert 0 ==  originalProject.dependencies.size()
+assert 'myprofile' == originalProject.profiles.profile.id.text()
+assert 1 == originalProject.profiles.profile.dependencies.size()
+
+File flattendPom = new File( basedir, '.flattened-pom.xml' )
+assert flattendPom.exists()
+
+def flattendProject = new XmlSlurper().parse( flattendPom )
+assert 0 ==  flattendProject.dependencies.size()
+assert 0 == flattendProject.profiles.profile.size()

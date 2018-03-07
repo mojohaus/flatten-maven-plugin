99e44309097a9c65737e7d576c11cee075458001
andye <andye@yambay.com>
2018-03-07 19:56:09 +0800

    Amended property name from flattenMode to flatten.mode as per @matthiasbalke request.

---
+++ src/test/java/org/codehaus/mojo/flatten/CmdLinePropertiesTest.java
@@ -0,0 +1,12 @@
+package org.codehaus.mojo.flatten;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Test;
+
+public class CmdLinePropertiesTest extends AbstractMojoTestCase {
+    @Test
+    public void test() throws Exception {
+//        System.out.println("Trying to look up mojo");
+//        FlattenMojo flattenMojo = (FlattenMojo) lookupMojo("flatten", "/Users/andye/Dev/projects/flatten-maven-plugin/pom.xml");
+    }
+}

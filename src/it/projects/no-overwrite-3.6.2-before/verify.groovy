mojohaus/flatten-maven-plugin

commit 6b5779f78774f2ab991f0583d2c0009717ecad06

Fix #140 by separating no-overwritten test into two basing on maven version (#154)
* fix #140 by separating no-overwritten test into two basing on maven version

* parse maven version by ./mvnw -v

* remove redundent code

Co-authored-by: yangnuoyu <yangnuoyu@penguin>

src/it/projects/no-overwrite-3.6.2-before/verify.groovy
@@ -16,9 +16,19 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+String mavenVersion = "./mvnw -v".execute().text.split()[2]
+String[] mavenVersionArray = mavenVersion.split("\\.")
+int[] versionArray = new int[3]
+for (int i = 0; i < 3; i++)
+    versionArray[i] = Integer.valueOf(mavenVersionArray[i])
+boolean isValid = versionArray[0] < 3\
+ || versionArray[0] == 3 && versionArray[1] < 6\
+ || versionArray[0] == 3 && versionArray[1] == 6 && versionArray[2] < 3
+if (isValid) {
+    File flattendPom = new File( basedir, '.flattened-pom.xml' )
+    assert flattendPom.exists()
+    long now = System.currentTimeMillis()
+    assert now - flattendPom.lastModified() > 20*1000
+}
 
-File flattendPom = new File( basedir, '.flattened-pom.xml' )
-assert flattendPom.exists()
-long now = System.currentTimeMillis()
-assert now - flattendPom.lastModified() > 20*1000
 

src/it/projects/no-overwrite-3.6.3-later/verify.groovy
@@ -1,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+String mavenVersion = "./mvnw -v".execute().text.split()[2]
+String[] mavenVersionArray = mavenVersion.split("\\.")
+int[] versionArray = new int[3]
+for (int i = 0; i < 3; i++)
+    versionArray[i] = Integer.valueOf(mavenVersionArray[i])
+boolean isValid = versionArray[0] > 3\
+ || versionArray[0] == 3 && versionArray[1] > 6\
+ || versionArray[0] == 3 && versionArray[1] == 6 && versionArray[2] >= 3
+if (isValid) {
+    File flattendPom = new File( basedir, '.flattened-pom.xml' )
+    assert flattendPom.exists()
+    long now = System.currentTimeMillis()
+    assert now - flattendPom.lastModified() > 20*1000
+}
+
+

src/it/projects/no-overwrite-3.6.2-before/.flattened-pom.xml
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns="http://maven.apache.org/POM/4.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.codehaus.mojo.flatten.its</groupId>
+  <artifactId>resolve-properties</artifactId>
+  <version>0.0.1-SNAPSHOT</version>
+  <dependencies>
+    <dependency>
+      <groupId>org.codehaus.mojo.flatten.its</groupId>
+      <artifactId>dep</artifactId>
+      <version>1.1</version>
+      <scope>compile</scope>
+    </dependency>
+  </dependencies>
+</project>

src/it/projects/no-overwrite-3.6.3-later/pom.xml
@@ -1,0 +1,19 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.codehaus.mojo.flatten.its</groupId>
+  <artifactId>resolve-properties</artifactId>
+  <version>0.0.1-SNAPSHOT</version>
+  
+  <properties>
+    <depVersion>1.1</depVersion>
+  </properties>
+  
+  <dependencies>
+    <dependency>
+      <groupId>org.codehaus.mojo.flatten.its</groupId>
+      <artifactId>dep</artifactId>
+      <version>${depVersion}</version>
+    </dependency>
+  </dependencies>
+  
+</project>

commit 4a38133c94208c820a5a2c1cb0aaa671e015c3d7

fixed Maven version detection using invoker.maven.version property
fixes issue introduced in #154 trying to fix #140

src/it/projects/no-overwrite-3.6.2-before/invoker.properties
@@ -1,0 +1,3 @@
+# .flattened-pom.xml contains xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+# xsd url changed to HTTPS in Maven 3.6.3 in https://issues.apache.org/jira/browse/MNG-6778
+invoker.maven.version = !3.6.3+

src/it/projects/no-overwrite-3.6.2-before/verify.groovy
@@ -16,21 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-String os = System.getProperty("os.name");
-String[] mavenWords = os.contains("Windows") ? "mvnw.cmd -v".execute().text.split() : "./mvnw -v".execute().text.split()
-String mavenVersion = mavenWords[1] == "Maven" ? mavenWords[2] : mavenWords[3]
-String[] mavenVersionArray = mavenVersion.split("\\.")
-int[] versionArray = new int[3]
-for (int i = 0; i < 3; i++)
-    versionArray[i] = Integer.valueOf(mavenVersionArray[i])
-boolean isValid = versionArray[0] < 3\
- || versionArray[0] == 3 && versionArray[1] < 6\
- || versionArray[0] == 3 && versionArray[1] == 6 && versionArray[2] < 3
-if (isValid) {
-    File flattendPom = new File( basedir, '.flattened-pom.xml' )
-    assert flattendPom.exists()
-    long now = System.currentTimeMillis()
-    assert now - flattendPom.lastModified() > 20*1000
-}
 
-
+File flattendPom = new File( basedir, '.flattened-pom.xml' )
+assert flattendPom.exists()
+long now = System.currentTimeMillis()
+assert now - flattendPom.lastModified() > 20*1000

src/it/projects/no-overwrite-3.6.3-later/invoker.properties
@@ -1,0 +1,3 @@
+# .flattened-pom.xml contains xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd"
+# xsd url changed to HTTPS in Maven 3.6.3 in https://issues.apache.org/jira/browse/MNG-6778
+invoker.maven.version = 3.6.3+

src/it/projects/no-overwrite-3.6.3-later/verify.groovy
@@ -17,21 +17,7 @@
  * under the License.
  */
 
-String os = System.getProperty("os.name").split()[0]
-String[] mavenWords = os.contains("Windows") ? "mvnw.cmd -v".execute().text.split() : "./mvnw -v".execute().text.split()
-String mavenVersion = mavenWords[1] == "Maven" ? mavenWords[2] : mavenWords[3]
-String[] mavenVersionArray = mavenVersion.split("\\.")
-int[] versionArray = new int[3]
-for (int i = 0; i < 3; i++)
-    versionArray[i] = Integer.valueOf(mavenVersionArray[i])
-boolean isValid = versionArray[0] > 3\
- || versionArray[0] == 3 && versionArray[1] > 6\
- || versionArray[0] == 3 && versionArray[1] == 6 && versionArray[2] >= 3
-if (isValid) {
-    File flattendPom = new File( basedir, '.flattened-pom.xml' )
-    assert flattendPom.exists()
-    long now = System.currentTimeMillis()
-    assert now - flattendPom.lastModified() > 20*1000
-}
-
-
+File flattendPom = new File( basedir, '.flattened-pom.xml' )
+assert flattendPom.exists()
+long now = System.currentTimeMillis()
+assert now - flattendPom.lastModified() > 20*1000
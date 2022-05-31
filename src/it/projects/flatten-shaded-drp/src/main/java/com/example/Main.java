971ae2858ceff3b45881135c90b27738df8da05c
Clay Jensen-Reimann <clayreimann@verizonmedia.com>
2022-05-31 10:11:33 -0500

    Set `project.originalModel` after flattening
    Based on the discussion in maven-shade-plugin#129 https://github.com/apache/maven-shade-plugin/pull/129#discussion_r843695691 It seems that the flatten plugin should update the `originalModel` property after we flatten the pom.

---
+++ src/it/projects/flatten-shaded-drp/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.List;
+import com.google.common.collect.ImmutableList;
+import org.apache.maven.archiver.MavenArchiver;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Object> l = ImmutableList.of();
+        MavenArchiver a = new MavenArchiver();
+    }
+}

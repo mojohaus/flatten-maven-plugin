85a71f397aacd91b9a2b72a7db9952de2e8de54b
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-03-27 19:11:40 +0200

    Add IT

---
+++ src/it/projects/issue-348-repository-from-project/verify.groovy
@@ -0,0 +1,12 @@
+
+import groovy.xml.XmlSlurper
+
+File flattendPom = new File( basedir, '.flattened-pom.xml' )
+assert flattendPom.exists()
+
+def flattendProject = new XmlSlurper().parse( flattendPom )
+assert 1 ==  flattendProject.repositories.size()
+
+assert 1 ==  flattendProject.dependencies.size()
+assert 'spring-core' ==  flattendProject.dependencies.dependency.artifactId.text()
+assert '6.0.0-M6' ==  flattendProject.dependencies.dependency.version.text()

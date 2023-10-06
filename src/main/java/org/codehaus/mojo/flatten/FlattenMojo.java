669a23174985c704edfb2ba20007122529c4599d
CedricMtta <>
2023-10-06 13:04:20 +0200

    #376 :bug: - Add external profiles and properties to the model building request

--- src/main/java/org/codehaus/mojo/flatten/FlattenMojo.java
+++ src/main/java/org/codehaus/mojo/flatten/FlattenMojo.java
@@ -74,6 +74,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
 import org.codehaus.mojo.flatten.cifriendly.CiInterpolator;
 import org.codehaus.mojo.flatten.model.resolution.FlattenModelResolver;
 import org.codehaus.plexus.util.StringUtils;
@@ -209,6 +210,12 @@ import org.xml.sax.ext.DefaultHandler2;
 public class FlattenMojo extends AbstractFlattenMojo {
 
     private static final int INITIAL_POM_WRITER_SIZE = 4096;
+
+    /**
+     * The {@link Settings} used to get active profile properties.
+     */
+    @Parameter(defaultValue = "${settings}", readonly = true, required = true)
+    private Settings settings;
 
     /**
      * The {@link MavenSession} used to get user properties.
@@ -393,7 +400,7 @@ public class FlattenMojo extends AbstractFlattenMojo {
      */
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        
         getLog().info("Generating flattened POM of project " + this.project.getId() + "...");
 
         inheritanceAssembler.flattenDependencyMode = this.flattenDependencyMode;
@@ -803,7 +810,13 @@ public class FlattenMojo extends AbstractFlattenMojo {
                 project.getRemoteProjectRepositories(),
                 getReactorModelsFromSession());
         Properties userProperties = this.session.getUserProperties();
+        settings.getProfiles()
+                .stream()
+                .filter(p -> settings.getActiveProfiles().contains(p.getId()))
+                .forEach(activeProfile -> userProperties.putAll(activeProfile.getProperties()));
+
         List<String> activeProfiles = this.session.getRequest().getActiveProfiles();
+        activeProfiles.addAll(this.settings.getActiveProfiles());
 
         return new DefaultModelBuildingRequest()
                 .setUserProperties(userProperties)
@@ -839,6 +852,7 @@ public class FlattenMojo extends AbstractFlattenMojo {
             final boolean embedBuildProfileDependencies,
             final FlattenMode flattenMode)
             throws MojoExecutionException {
+
         ModelBuildingResult buildingResult;
         try {
             ProfileInjector customInjector = (model, profile, request, problems) -> {

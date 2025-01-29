d13e078811d9b589ae05a76494ae9702bebef657
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2025-01-29 10:56:31 +0100

    Bind :clean to 'clean' phase by default

--- src/main/java/org/codehaus/mojo/flatten/CleanMojo.java
+++ src/main/java/org/codehaus/mojo/flatten/CleanMojo.java
@@ -23,6 +23,7 @@ import java.io.File;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -37,6 +38,7 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(
         name = "clean",
+        defaultPhase = LifecyclePhase.CLEAN,
         requiresProject = true,
         requiresDirectInvocation = false,
         executionStrategy = "once-per-session",

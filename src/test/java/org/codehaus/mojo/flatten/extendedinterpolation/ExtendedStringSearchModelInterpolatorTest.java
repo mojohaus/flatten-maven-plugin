8b48ce7cc63c587770d1c2585362663835cef591
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-06-13 19:18:52 +0200

    Fix FilteringValueSourceWrapper for a newer version of plexus-interpolation, Maven 3.9.10
    fix #451

---
+++ src/test/java/org/codehaus/mojo/flatten/extendedinterpolation/ExtendedStringSearchModelInterpolatorTest.java
@@ -0,0 +1,36 @@
+package org.codehaus.mojo.flatten.extendedinterpolation;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.DefaultModelBuildingRequest;
+import org.apache.maven.model.building.ModelProblemCollectorRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExtendedStringSearchModelInterpolatorTest {
+
+    @Test
+    public void testExtendedStringSearchModelInterpolator() throws Exception {
+        ExtendedModelInterpolator interpolator = new ExtendedStringSearchModelInterpolator();
+
+        Model efectiveModel = new Model();
+        efectiveModel.addProperty("foo", "bar");
+
+        Model model = new Model();
+        model.setArtifactId("${foo}");
+        model.setDescription("${basedir}");
+
+        List<ModelProblemCollectorRequest> problems = new ArrayList<>();
+
+        interpolator.interpolateModel(
+                efectiveModel, model, new File("."), new DefaultModelBuildingRequest(), problems::add);
+
+        assertEquals(0, problems.size());
+        assertEquals("bar", model.getArtifactId());
+        assertEquals("${basedir}", model.getDescription());
+    }
+}

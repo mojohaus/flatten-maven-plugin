5344031b765fb717ed24b5e066ccb46879f4a8a9
HelloDhero <7644567+HelloDhero@users.noreply.github.com>
2025-06-22 19:11:14 +0800

    Update FlattenMojo.java

--- src/main/java/org/codehaus/mojo/flatten/FlattenMojo.java
+++ src/main/java/org/codehaus/mojo/flatten/FlattenMojo.java
@@ -29,6 +29,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
